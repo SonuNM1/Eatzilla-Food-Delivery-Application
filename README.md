73d0b56924029dcae72729388544625738b0b3fd
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-09-05 23:53:22 +0530

    5 Sept

--- README.md
+++ README.md
@@ -12,3 +12,5 @@
 - Schema Vs. Model 
 
 - mongoImport - MongoDB Database tools 
+
+- express-validator : validate user input. Helps ensure that the data received from a client (such as from a form submission or an API req) meets certain criteria before it's processed by our server
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 5000 ;
 
 connectDB() ; 
 
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000") ; 
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    ) ; 
+    next()
+})
+
 app.use(express.json()) ; // Middleware to parse JSON bodies
 
 app.use('/api', require('./routes/CreateUser'))

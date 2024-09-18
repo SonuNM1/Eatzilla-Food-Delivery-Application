060a1acd249b47004efeb3dd7bf465123319f53a
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-09-18 10:54:02 +0530

    18 Sept

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ connectDB();
 
 app.use(cors({
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
 }))
 
 /*
--- backend/routes/DisplayData.js
+++ backend/routes/DisplayData.js
@@ -7,14 +7,19 @@ router.post('/foodData', (req, res) => {
         console.log('Food Category:', global.foodCategory);  // Debug log
 
         if (global.food_items && global.foodCategory) {
-            res.send([global.food_items, global.foodCategory]);
+            // Remove any duplicates from food_items and foodCategory if needed
+            const uniqueFoodItems = [...new Map(global.food_items.map(item => [item._id, item])).values()];
+            const uniqueFoodCategories = [...new Map(global.foodCategory.map(cat => [cat._id, cat])).values()];
+
+            res.send([uniqueFoodItems, uniqueFoodCategories]);
         } else {
-            res.send("Data not loaded from the database yet, please try again later.");
+            res.status(404).send("Data not loaded from the database yet, please try again later.");
         }
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Server error");
     }
 });
+
 
 module.exports = router;

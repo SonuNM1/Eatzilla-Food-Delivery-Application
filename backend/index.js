aa5f8e8997fb68b72c397021bbbd6ab2bc13ffe9
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-09-19 13:45:33 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://eatzilla.netlify.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
 }))
